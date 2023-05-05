04e5f8d752efd0f3a21f9e8822f5733f37bb76cc
gosekose <ksyn1611@naver.com>
2023-05-06 03:14:08 +0900

    재귀 조합

--- src/combination/Combination.java
+++ src/combination/Combination.java
@@ -15,20 +15,20 @@ public class Combination {
         List<Integer> arr = new ArrayList<>();
         for (int i = 0; i < n; i++) arr.add(i + 1);
 
-        getCombinationHelper(arr, 3, 0, new ArrayList<>(), combi);
+        getCombinationHelper(arr, 3, 0, new ArrayList<>());
 
         return combi;
     }
 
-    void getCombinationHelper(List<Integer> input, int k, int start, List<Integer> current, List<List<Integer>> result) {
+    void getCombinationHelper(List<Integer> input, int k, int start, List<Integer> current) {
         if (k == 0) {
-            result.add(new ArrayList<>(current));
+            combi.add(new ArrayList<>(current));
             return;
         }
 
         for (int i = start; i < input.size(); i++) {
             current.add(input.get(i));
-            getCombinationHelper(input, k - 1, i + 1, current, result);
+            getCombinationHelper(input, k - 1, i + 1, current);
             current.remove(current.size() - 1);
         }
     }
@@ -39,8 +39,13 @@ class CombinationTest {
     public static void main(String[] args) {
         Combination combination = new Combination( 8 * 8);
         List<List<Integer>> combi = combination.getCombination();
+        System.out.println(combi.size());
         System.out.println(combi.get(6).get(0));
         System.out.println(combi.get(6).get(1));
         System.out.println(combi.get(6).get(2));
+
+        System.out.println(combi.get(0).get(0));
+        System.out.println(combi.get(0).get(1));
+        System.out.println(combi.get(0).get(2));
     }
 }
